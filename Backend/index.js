9fb7dc92abb89c354ca7f86cad1b0a66d2abd8ba
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-07-19 13:52:19 +0530

    ok

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const cors = require("cors")
+require("dotenv").config()
+const port = process.env.PORT
+const cookiParser = require("cookie-parser");
+const connectDB = require("./db");
 const app = express();
+const bodyParser = require('body-parser');
 
-require("dotenv").config();
+app.use(express.json())
+app.use(cors())
+
+const userrouter = require("./Routes/user.router")
+
+const auth = require("./Middleware/auth")
+
+app.use(cookiParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/user", userrouter)
+
 app.listen(process.env.PORT, () => {
+  connectDB();
   console.log("server is running at 8000");
 });

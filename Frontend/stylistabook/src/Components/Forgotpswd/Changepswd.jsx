a222659b21e93b585886aa5c23ce0a65de9c2a5e
haseeb1233 <haseebshs@gmail.com>
2023-07-19 23:27:07 +0530

    login/signup

---
+++ Frontend/stylistabook/src/Components/Forgotpswd/Changepswd.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Changepswd.css"
+function Changepswd() {
+  return (
+    <div className='Changepswd'>
+      <div>
+        <button className='btns'>Sign Up</button>
+        <button style={{backgroundColor:"black",color:"white"}} className='btns'>Login</button>
+      </div>
+      <form className='loginform' action="">
+        <input type="number" placeholder='Enter Otp' />
+        <input type="password" placeholder='Password'/>
+        <input type="password" placeholder='Confirm Password'/>
+        <input id='submitformbtn' type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Changepswd

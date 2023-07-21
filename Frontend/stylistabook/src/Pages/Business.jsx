756cc1e91bbde024d1727ac9491d7c062a395474
harpreet-1 <hpsllg8@gmail.com>
2023-07-21 11:01:10 +0530

    day-2-done

--- Frontend/stylistabook/src/Pages/Business.jsx
+++ Frontend/stylistabook/src/Pages/Business.jsx
@@ -2,34 +2,35 @@ import React from "react";
 import BusinessNavbar from "../Components/BusinessNavbar";
 // import { Link } from "react-router-dom";
 import styles from "../css/BusnessDashboard.module.css";
+import BDHighlights from "../Components/BDHighlights";
 
 function Bussiness() {
   return (
     <>
       <BusinessNavbar />
       <div className={styles.mainBody}>
-        <div className={styles.dashbord_header}>
-          <div>Dashboard</div>
+        <BDHighlights />
+        <div className={styles.today_appointments}>
+          <div className={styles.TD_header}>
+            <h3>Today Appointments</h3>
+          </div>
+          <div className={styles.TDA_body}>
+            <div className={styles.TDA_headings}>
+              <div>TIME</div>
+              <div>CUSTOMORE NAME</div>
+              <div>SERVICE</div>
+              <div>PRICE</div>
+              <div>STATUS</div>
+            </div>
+            <div className={styles.TDA_card}>
+              <div>08:00 PM</div>
+              <div>Harry</div>
+              <div>Hair Cut</div>
+              <div>400</div>
+              <div>Pending</div>
+            </div>
+          </div>
         </div>
-
-        <div className={styles.dashboard_highlights}>
-          <div>
-            <p>2</p>
-            <p>APPOINTMENT</p>
-          </div>
-          <div>
-            <p>RS .600.00</p>
-            <p>ESTIMATED SALES</p>
-          </div>
-          <div>
-            <p>3</p>
-            <p>CUSTMORES</p>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.today_appointments}>
-        <div className={styles.today_appointments_header}></div>
       </div>
     </>
   );

a4efca42975da5e715522ff957687e090eb0e97d
harpreet-1 <hpsllg8@gmail.com>
2023-07-22 08:51:02 +0530

    day-5-chnages

--- Frontend/stylistabook/src/Pages/BSprofile.jsx
+++ Frontend/stylistabook/src/Pages/BSprofile.jsx
@@ -6,7 +6,21 @@ function BSprofile() {
     <>
       <BusinessNavbar />
       <div className={styles.profileBody}>
-        <h1>hello</h1>
+        <div className={styles.profile_header}>
+          <div>
+            <div className={styles.BS_name}>
+              <h2>Salon Shop</h2>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img
+                src="https://d2zdpiztbgorvt.cloudfront.net/region1/us/864939/biz_photo/6b52dd77f1614986a63f5789d1fc1c-angela-s-beauty-palace-biz-photo-9488841906f84b3c945e8382f6c99f-booksy.jpeg?size=640x427"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );

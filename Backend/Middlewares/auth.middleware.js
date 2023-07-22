4446928d2bb39d3b869ce4eb318d3a0649307fb6
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-07-22 15:19:00 +0530

    ok run

--- Backend/Middlewares/auth.middleware.js
+++ Backend/Middlewares/auth.middleware.js
@@ -1,22 +1,50 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const authentication = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization?.split(' ')[1];
 
-    if (!token) {
-        return res.status(401).json({ error: "Unauthorized" });
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized: Token not provided." });
+  }
+
+  jwt.verify(token, 'styleSync', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: "Unauthorized: Invalid token." });
     }
+    
+    // Optionally, you can attach the decoded data to the request object for later use.
+    req.user = decoded;
+    next();
+  });
+};
 
-    jwt.verify(token, 'styleSync',
+module.exports = { authentication };
 
-    function(err,decoded){
-        if(err){
-            res.send(err)
-        }
-        if(decoded){
-            next()
-        }
-    })
 
-}
-module.exports = { authentication }
+
+
+
+
+//old//
+// const jwt = require("jsonwebtoken")
+
+// const authentication = (req, res, next) => {
+//     const token = req.headers.authorization.split(' ')[1];
+
+//     if (!token) {
+//         return res.status(401).json({ error: "Unauthorized" });
+//     }
+
+//     jwt.verify(token, 'styleSync',
+
+//     function(err,decoded){
+//         if(err){
+//             res.send(err)
+//         }
+//         if(decoded){
+//             next()
+//         }
+//     })
+
+// }
+// module.exports = { authentication }
---
+++ Backend/Routes/stylist.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { StylisttModel } = require("../Models/stylist.model");
+
+
+const stylistRouter = express.Router();
+
+// code here //
+
+module.exports = {
+    stylistRouter
+};

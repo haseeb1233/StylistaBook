fe96e330523802fb89828c096e478612eae2a04a
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-07-22 16:06:40 +0530

    ok

--- Backend/index.js
+++ Backend/index.js
@@ -4,21 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 
 const { connection } = require('./Configs/db');
-const { styleRouter } = require('./Routes/style.routes');
-const { stylistRouter } = require('./Routes/stylist.routes');
+
 const userRoutes = require("./Routes/user.routes");
-const { appointmentRouter } = require('./Routes/appointment.routes');
-const { adminRouter } = require('./Routes/admin.routes');
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRoutes)
-app.use('/style', styleRouter)
-app.use('/stylist', stylistRouter)
-app.use('/appointment', appointmentRouter)
-app.use('/admin', adminRouter)
+
 
 
 const Port = process.env.Port || 8000;
